4e64fd37cb1df70bf9b2697ee0c703bbaa323d11
Natalia Krzysztoporska <s32521@pjwstk.edu.pl>
2025-04-17 14:20:24 +0200

    zad

---
+++ Vehicle.java
@@ -0,0 +1,8 @@
+public class Vehicle {
+    public void start(){
+        System.out.println("pojazd uruchomiony");
+    }
+    public void stop(){
+        System.out.println("pojazd zatrzymany");
+    }
+}
